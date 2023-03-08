38e96fbbcf2e453a993abb94e99be4adb5e1ed58
exejar <56853437+exejar@users.noreply.github.com>
2023-03-07 20:12:35 -0500

    Add instructions to cancel event

--- src/main/kotlin/club/maxstats/weave/loader/hooks/GuiOpenEventHook.kt
+++ src/main/kotlin/club/maxstats/weave/loader/hooks/GuiOpenEventHook.kt
@@ -1,30 +1,42 @@
 package club.maxstats.weave.loader.hooks
 
 import club.maxstats.weave.loader.api.HookManager
-import club.maxstats.weave.loader.api.event.Event
-import club.maxstats.weave.loader.api.event.EventBus
+import club.maxstats.weave.loader.api.event.*
 import club.maxstats.weave.loader.util.asm
 import club.maxstats.weave.loader.util.getSingleton
 import club.maxstats.weave.loader.util.internalNameOf
 import club.maxstats.weave.loader.util.named
+import net.minecraft.client.gui.GuiScreen
+import org.objectweb.asm.tree.LabelNode
 
 fun HookManager.registerGuiOpenHook() = register("net/minecraft/client/Minecraft") {
     node.methods.named("displayGuiScreen").instructions.insert(asm {
-        getSingleton<EventBus>()
-
-        new("club/maxstats/weave/loader/api/event/GuiOpenEvent")
+        new(internalNameOf<GuiOpenEvent>())
+        dup
         dup
         aload(1)
         invokespecial(
-            "club/maxstats/weave/loader/api/event/GuiOpenEvent",
+            internalNameOf<GuiOpenEvent>(),
             "<init>",
             "(Lnet/minecraft/client/gui/GuiScreen;)V"
         )
+        getSingleton<EventBus>()
+        swap
 
         invokevirtual(
             internalNameOf<EventBus>(),
             "callEvent",
             "(L${internalNameOf<Event>()};)V"
         )
+
+        val end = LabelNode()
+
+        invokevirtual(internalNameOf<CancellableEvent>(), "isCancelled", "()Z")
+        ifeq(end)
+
+        _return
+
+        +end
+        f_same()
     })
 }
