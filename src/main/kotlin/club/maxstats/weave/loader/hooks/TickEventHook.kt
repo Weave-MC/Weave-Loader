a6dfb3886c2d340b8d25ad9e2cb5c8198ac1d67a
exejar <56853437+exejar@users.noreply.github.com>
2023-03-07 20:11:08 -0500

    Removed string class references and use internalNameOf<Class>() instead

--- src/main/kotlin/club/maxstats/weave/loader/hooks/TickEventHook.kt
+++ src/main/kotlin/club/maxstats/weave/loader/hooks/TickEventHook.kt
@@ -3,6 +3,7 @@ package club.maxstats.weave.loader.hooks
 import club.maxstats.weave.loader.api.HookManager
 import club.maxstats.weave.loader.api.event.Event
 import club.maxstats.weave.loader.api.event.EventBus
+import club.maxstats.weave.loader.api.event.TickEvent
 import club.maxstats.weave.loader.util.asm
 import club.maxstats.weave.loader.util.getSingleton
 import club.maxstats.weave.loader.util.internalNameOf
@@ -10,9 +11,9 @@ import club.maxstats.weave.loader.util.named
 
 fun HookManager.registerTickHook() = register("net/minecraft/client/Minecraft") {
     node.methods.named("runTick").instructions.insert(asm {
-        new("club/maxstats/weave/loader/api/event/TickEvent")
+        new(internalNameOf<TickEvent>())
         dup
-        invokespecial("club/maxstats/weave/loader/api/event/TickEvent", "<init>", "()V")
+        invokespecial(internalNameOf<TickEvent>(), "<init>", "()V")
         getSingleton<EventBus>()
         swap
         invokevirtual(
