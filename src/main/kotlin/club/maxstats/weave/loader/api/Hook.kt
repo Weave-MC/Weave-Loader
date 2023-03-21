de9331f563333d96690407422f0a30a82aa2f1e0
Sam <90007553+Scherso@users.noreply.github.com>
2023-03-21 09:18:08 -0400

    Update Hook.kt

--- src/main/kotlin/club/maxstats/weave/loader/api/Hook.kt
+++ src/main/kotlin/club/maxstats/weave/loader/api/Hook.kt
@@ -3,9 +3,11 @@ package club.maxstats.weave.loader.api
 import org.objectweb.asm.tree.ClassNode
 
 abstract class Hook(val targetClassName: String) {
+
     abstract fun transform(node: ClassNode, cfg: AssemblerConfig)
 
     abstract class AssemblerConfig {
         abstract fun computeFrames()
     }
+    
 }
