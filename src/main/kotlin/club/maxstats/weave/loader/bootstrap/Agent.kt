4e84c201caff205276f57bbfaa5a5addd0b8491a
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-03-24 22:56:23 +0100

    Version detection

--- src/main/kotlin/club/maxstats/weave/loader/bootstrap/Agent.kt
+++ src/main/kotlin/club/maxstats/weave/loader/bootstrap/Agent.kt
@@ -4,12 +4,18 @@ import java.lang.instrument.Instrumentation
 
 @Suppress("UNUSED_PARAMETER")
 public fun premain(opt: String?, inst: Instrumentation) {
+    val mcVersion = findVersion()
+    if(mcVersion != "1.8.9") {
+        println("[Weave] $mcVersion not supported, disabling...")
+        return
+    }
+
     inst.addTransformer(object : SafeTransformer {
         override fun transform(loader: ClassLoader, className: String, originalClass: ByteArray): ByteArray? {
             if (className.startsWith("net/minecraft/")) {
                 inst.removeTransformer(this)
-                
-                /* 
+
+                /*
                  * Load the rest of the loader using Lunar's own class loader.
                  * This allows us access to Minecraft's classes throughout the project.
                  */
@@ -23,3 +29,9 @@ public fun premain(opt: String?, inst: Instrumentation) {
         }
     })
 }
+
+private fun findVersion() =
+    """--version ([^ ]+)""".toRegex()
+        .find(System.getProperty("sun.java.command"))
+        ?.groupValues?.get(1)
+
