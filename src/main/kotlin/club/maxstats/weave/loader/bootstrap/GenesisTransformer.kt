5c46c0a7e5e264b3634347364131536d01f247fb
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-03-26 20:43:50 +0200

    noClassCache hack

--- src/main/kotlin/club/maxstats/weave/loader/bootstrap/GenesisTransformer.kt
+++ src/main/kotlin/club/maxstats/weave/loader/bootstrap/GenesisTransformer.kt
@@ -1,43 +1,36 @@
 package club.maxstats.weave.loader.bootstrap
 
-import club.maxstats.weave.loader.api.util.asm
 import club.maxstats.weave.loader.util.named
-import club.maxstats.weave.loader.util.next
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.LdcInsnNode
-import org.objectweb.asm.tree.MethodInsnNode
 
 internal object GenesisTransformer : SafeTransformer {
     override fun transform(
         loader: ClassLoader,
         className: String,
         originalClass: ByteArray
-    ) = if (className == "com/moonsworth/lunar/genesis/Genesis") originalClass.modify { node ->
-        val method = node.methods.named("main")
-        val ldc = method.instructions.first { it is LdcInsnNode && it.cst == "prebake.cache" }
+    ): ByteArray? {
+        if (className != "com/moonsworth/lunar/genesis/Genesis") return null
 
-        method.instructions.insert(
-            ldc.next<MethodInsnNode> { it.name == "exists" },
-            asm {
-                pop
-                iconst_0
-            }
-        )
+        val cn = ClassNode()
+        val cr = ClassReader(originalClass)
+        cr.accept(cn, 0)
 
-        val prebake = node.methods.named("prebake")
-        prebake.instructions = asm { _return }
-        prebake.exceptions.clear()
-        prebake.tryCatchBlocks.clear()
-        prebake.localVariables.clear()
-    } else null
-}
+        /*
+        * replace noClassCache with a flag that always gets passed
+        * done this way because it'll throw if/when lunar removes --noClassCache
+        * */
 
-private fun ByteArray.modify(handler: (node: ClassNode) -> Unit): ByteArray {
-    val reader = ClassReader(this)
-    val node = ClassNode().also { reader.accept(it, 0) }.also(handler)
-    val writer = ClassWriter(reader, 0)
-    node.accept(writer)
-    return writer.toByteArray()
+        cn.methods.named("main").instructions
+            .filterIsInstance<LdcInsnNode>()
+            .first { it.cst == "noClassCache" }
+            .cst = "gameDir"
+
+        val cw = ClassWriter(cr, 0)
+        cn.accept(cw)
+
+        return cw.toByteArray()
+    }
 }
