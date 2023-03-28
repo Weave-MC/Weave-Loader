ccb8b7d939928b953306d94c8d301ce46076cccf
exejar <56853437+exejar@users.noreply.github.com>
2023-03-28 17:39:58 -0400

    Fixed ClassLoader issues. Mixins are initialized in Ichor and Weave is initialized in Genesis.

--- src/main/kotlin/club/maxstats/weave/loader/WeaveLoader.kt
+++ src/main/kotlin/club/maxstats/weave/loader/WeaveLoader.kt
@@ -3,16 +3,7 @@ package club.maxstats.weave.loader
 import club.maxstats.weave.loader.api.HookManager
 import club.maxstats.weave.loader.api.ModInitializer
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.decodeFromStream
-import org.spongepowered.asm.launch.MixinBootstrap
-import org.spongepowered.asm.mixin.Mixins
 import java.lang.instrument.Instrumentation
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
-import java.util.jar.JarFile
 import kotlin.io.path.*
 
 public object WeaveLoader {
@@ -23,46 +14,21 @@ public object WeaveLoader {
      */
     @JvmStatic
     @OptIn(ExperimentalSerializationApi::class)
-    public fun preInit(inst: Instrumentation, classLoader: ClassLoader) {
+    public fun preInit(inst: Instrumentation, entryPoints: List<String>) {
+        println("[Weave] Initializing Weave")
         inst.addTransformer(hookManager.Transformer())
 
-        MixinBootstrap.init()
+        entryPoints.forEach {
+            val instance = WeaveLoader::class.java.classLoader.loadClass(it)
+                .getConstructor()
+                .newInstance() as? ModInitializer
+                ?: error("[Weave] $it does not implement ModInitializer!")
 
-        getOrCreateModDirectory()
-            .listDirectoryEntries("*.jar")
-            .filter { it.isRegularFile() }
-            .map { it.toFile() }
-            .forEach { modFile ->
-                println("[Weave] Loading ${modFile.name}")
-                val jar = JarFile(modFile)
-                inst.appendToSystemClassLoaderSearch(jar)
+            instance.preInit(hookManager)
+        }
 
-                val config = Json.decodeFromStream<WeaveModConfig>(
-                    jar.getInputStream(
-                        jar.getEntry("weave.mod.json") ?: error("${modFile.name} does not contain a weave.mod.json!")
-                    )
-                )
-
-                config.mixins.forEach { Mixins.addConfiguration(it) }
-
-                config.entrypoints.forEach {
-                    val instance = classLoader.loadClass(it)
-                        .getConstructor()
-                        .newInstance() as? ModInitializer
-                        ?: error("$it (mod ${modFile.name} does not implement ModInitializer!")
-
-                    instance.preInit(hookManager)
-                }
-            }
-    }
-
-    private fun getOrCreateModDirectory(): Path {
-        val dir = Paths.get(System.getProperty("user.home"), ".lunarclient", "mods")
-        if (dir.exists() && !dir.isDirectory()) Files.delete(dir)
-        if (!dir.exists()) dir.createDirectory()
-        return dir
+        println("[Weave] Initialized Weave")
     }
 }
 
-@Serializable
-private data class WeaveModConfig(val mixins: List<String> = listOf(), val entrypoints: List<String>)
+
--- src/main/kotlin/club/maxstats/weave/loader/bootstrap/Agent.kt
+++ src/main/kotlin/club/maxstats/weave/loader/bootstrap/Agent.kt
@@ -1,6 +1,16 @@
 package club.maxstats.weave.loader.bootstrap
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import org.spongepowered.asm.launch.MixinBootstrap
+import org.spongepowered.asm.mixin.Mixins
 import java.lang.instrument.Instrumentation
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.jar.JarFile
+import kotlin.io.path.*
 
 @Suppress("UNUSED_PARAMETER")
 public fun premain(opt: String?, inst: Instrumentation) {
@@ -14,14 +24,35 @@ public fun premain(opt: String?, inst: Instrumentation) {
             if (className.startsWith("net/minecraft/")) {
                 inst.removeTransformer(this)
 
-                /*
-                 * Load the rest of the loader using Lunar's own class loader.
-                 * This allows us access to Minecraft's classes throughout the project.
-                 */
+                val configList = mutableListOf<WeaveModConfig>()
+                getOrCreateModDirectory()
+                    .listDirectoryEntries("*.jar")
+                    .filter { it.isRegularFile() }
+                    .map { it.toFile() }
+                    .forEach { modFile ->
+                        println("[Weave] Loading ${modFile.name}")
+                        val jar = JarFile(modFile)
+                        inst.appendToSystemClassLoaderSearch(jar)
 
-                loader.loadClass("club.maxstats.weave.loader.WeaveLoader")
-                    .getDeclaredMethod("preInit", Instrumentation::class.java, ClassLoader::class.java)
-                    .invoke(null, inst, loader)
+                        val config = Json.decodeFromStream<WeaveModConfig>(
+                            jar.getInputStream(
+                                jar.getEntry("weave.mod.json") ?: error("${modFile.name} does not contain a weave.mod.json!")
+                            )
+                        )
+                        configList += config
+                    }
+
+                loader.loadClass("club.maxstats.weave.loader.bootstrap.MixinLoader")
+                    .getConstructor(List::class.java)
+                    .newInstance(configList.flatMap { it.mixins })
+
+                /*
+                Load the rest of the loader using Genesis class loader.
+                This allows us to access Minecraft's classes throughout the project.
+                */
+                loader.parent.loadClass("club.maxstats.weave.loader.WeaveLoader")
+                    .getDeclaredMethod("preInit", Instrumentation::class.java, List::class.java)
+                    .invoke(null, inst, configList.flatMap { it.entrypoints })
             }
 
             return null
@@ -31,8 +62,28 @@ public fun premain(opt: String?, inst: Instrumentation) {
     inst.addTransformer(GenesisTransformer)
 }
 
+private fun getOrCreateModDirectory(): Path {
+    val dir = Paths.get(System.getProperty("user.home"), ".lunarclient", "mods")
+    if (dir.exists() && !dir.isDirectory()) Files.delete(dir)
+    if (!dir.exists()) dir.createDirectory()
+    return dir
+}
+
 private fun findVersion() =
     """--version ([^ ]+)""".toRegex()
         .find(System.getProperty("sun.java.command"))
         ?.groupValues?.get(1)
 
+@Serializable
+private data class WeaveModConfig(val mixins: List<String> = listOf(), val entrypoints: List<String>)
+
+public class MixinLoader(mixins: List<String>) {
+    init {
+        println("[Weave] Initializing Mixins")
+
+        MixinBootstrap.init()
+        mixins.forEach { Mixins.addConfiguration(it) }
+
+        println("[Weave] Initialized Mixins")
+    }
+}
