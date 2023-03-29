49fc69bb6d92f21324f7346220b7154b70ede558
exejar <56853437+exejar@users.noreply.github.com>
2023-03-29 13:36:16 -0400

    Added Post hook to RenderLivingEvent

--- src/main/kotlin/club/maxstats/weave/loader/hooks/RenderLivingEventHook.kt
+++ src/main/kotlin/club/maxstats/weave/loader/hooks/RenderLivingEventHook.kt
@@ -7,12 +7,14 @@ import club.maxstats.weave.loader.api.util.asm
 import club.maxstats.weave.loader.util.callEvent
 import club.maxstats.weave.loader.util.internalNameOf
 import club.maxstats.weave.loader.util.named
+import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.LabelNode
 
 internal class RenderLivingEventHook : Hook("net/minecraft/client/renderer/entity/RendererLivingEntity") {
     override fun transform(node: ClassNode, cfg: AssemblerConfig) {
-        node.methods.named("doRender").instructions.insert(asm {
+        val mn = node.methods.named("doRender")
+        mn.instructions.insert(asm {
             new(internalNameOf<RenderLivingEvent.Pre>())
             dup
             dup
@@ -43,6 +45,28 @@ internal class RenderLivingEventHook : Hook("net/minecraft/client/renderer/entit
 
             +end
             f_same()
+        })
+        mn.instructions.insertBefore(mn.instructions.findLast { it.opcode == Opcodes.RETURN }, asm {
+            new(internalNameOf<RenderLivingEvent.Post>())
+            dup
+            dup
+            aload(0)
+            aload(1)
+            dload(2)
+            dload(4)
+            dload(6)
+            fload(9)
+            invokespecial(
+                internalNameOf<RenderLivingEvent.Post>(),
+                "<init>",
+                "(Lnet/minecraft/client/renderer/entity/RendererLivingEntity;" +
+                    "Lnet/minecraft/entity/EntityLivingBase;" +
+                    "D" +
+                    "D" +
+                    "D" +
+                    "F)V"
+            )
+            callEvent()
         })
     }
 }
