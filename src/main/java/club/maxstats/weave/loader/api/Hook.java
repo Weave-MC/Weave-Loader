8b0e02cac13f5cc322e95cfdf0cd7e6af32c505e
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-04-02 18:27:48 +0200

    @NotNull

--- src/main/java/club/maxstats/weave/loader/api/Hook.java
+++ src/main/java/club/maxstats/weave/loader/api/Hook.java
@@ -1,13 +1,14 @@
 package club.maxstats.weave.loader.api;
 
 import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.objectweb.asm.tree.ClassNode;
 
 @AllArgsConstructor
 public abstract class Hook {
     public final String targetClassName;
 
-    public abstract void transform(ClassNode node, AssemblerConfig cfg);
+    public abstract void transform(@NotNull ClassNode node, @NotNull AssemblerConfig cfg);
 
     public interface AssemblerConfig {
         void computeFrames();
--- src/main/java/club/maxstats/weave/loader/api/command/Command.java
+++ src/main/java/club/maxstats/weave/loader/api/command/Command.java
@@ -1,5 +1,7 @@
 package club.maxstats.weave.loader.api.command;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Arrays;
 
 public abstract class Command {
@@ -11,7 +13,7 @@ public abstract class Command {
         this.aliases = aliases;
     }
 
-    public abstract void handle(String[] args);
+    public abstract void handle(@NotNull String[] args);
 
     boolean matches(String s) {
         return name.equalsIgnoreCase(s) || Arrays.stream(aliases).anyMatch(alias -> alias.equalsIgnoreCase(s));
