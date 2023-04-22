660b5182f71625df53df7d643b5fd7e5e715baeb
Candicey <13133556-Candicey@users.noreply.gitlab.com>
2023-04-23 02:37:38 +0700

    Add InsnList#insertBeforeReturn(InsnList) extension

--- src/main/kotlin/net/weavemc/loader/util/Extensions.kt
+++ src/main/kotlin/net/weavemc/loader/util/Extensions.kt
@@ -1,11 +1,9 @@
 package net.weavemc.loader.util
 
 import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Opcodes
 import org.objectweb.asm.Type
-import org.objectweb.asm.tree.AbstractInsnNode
-import org.objectweb.asm.tree.ClassNode
-import org.objectweb.asm.tree.FieldNode
-import org.objectweb.asm.tree.MethodNode
+import org.objectweb.asm.tree.*
 import java.io.File
 
 internal fun List<MethodNode>.named(name: String) = find { it.name == name }!!
@@ -29,6 +27,11 @@ internal inline fun <reified T : AbstractInsnNode> AbstractInsnNode.prev(p: (T) 
         insn = insn.previous ?: return null
         if (insn is T && p(insn)) return insn
     }
+}
+
+internal fun InsnList.insertBeforeReturn(insnList: InsnList) {
+    val returnNode = findLast { it.opcode == Opcodes.RETURN } ?: return
+    insertBefore(returnNode, insnList)
 }
 
 internal fun ClassNode.dump(file: String) {
