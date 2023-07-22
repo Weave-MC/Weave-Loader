db72024046aeba5a9f99546eab40302dabf4d573
exejar <56853437+exejar@users.noreply.github.com>
2023-07-22 18:00:59 -0400

    Set java version to 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.0" apply false
     kotlin("plugin.serialization") version "1.9.0" apply false
     kotlin("plugin.lombok") version "1.9.0" apply false
+}
+
+subprojects {
+    tasks.withType<JavaCompile> {
+        targetCompatibility = "1.8"
+        sourceCompatibility = "1.8"
+    }
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
