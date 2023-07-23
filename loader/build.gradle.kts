474ae7f3490a6846adfc429bfc9bbfde48a1ee55
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-07-23 19:37:33 +0200

    shadowjar

--- loader/build.gradle.kts
+++ loader/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.serialization")
     kotlin("plugin.lombok")
     `maven-publish`
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 repositories {
@@ -17,13 +18,17 @@ dependencies {
 }
 
 tasks.jar {
-    from({ configurations.runtimeClasspath.get().map { zipTree(it) } }) {
-        exclude("**/module-info.class")
-    }
-
     manifest.attributes(
         "Premain-Class" to "net.weavemc.loader.bootstrap.AgentKt"
     )
+}
+
+tasks.shadowJar {
+    relocate("org.objectweb.asm", "net.weavemc.asm")
+}
+
+tasks.assemble {
+    dependsOn("shadowJar")
 }
 
 publishing {
