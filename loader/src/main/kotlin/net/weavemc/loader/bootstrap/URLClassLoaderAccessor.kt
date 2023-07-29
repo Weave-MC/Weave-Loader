6adf5f595e8dc46cd90672665d9a5bd07addc323
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-07-29 18:19:45 +0200

    Force asm to be loaded in the system classloader

--- loader/src/main/kotlin/net/weavemc/loader/bootstrap/URLClassLoaderAccessor.kt
+++ loader/src/main/kotlin/net/weavemc/loader/bootstrap/URLClassLoaderAccessor.kt
@@ -1,11 +1,14 @@
 package net.weavemc.loader.bootstrap
 
+import net.weavemc.weave.api.bytecode.asm
 import net.weavemc.weave.api.bytecode.internalNameOf
 import net.weavemc.weave.api.bytecode.visitAsm
 import org.objectweb.asm.ClassReader
-import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.LabelNode
+import org.objectweb.asm.tree.MethodNode
 import java.net.URL
 import java.net.URLClassLoader
 
@@ -18,29 +21,36 @@ object URLClassLoaderTransformer : SafeTransformer {
         val cr = ClassReader(originalClass)
         if(cr.superName != internalNameOf<URLClassLoader>()) return null
 
+        val cn = ClassNode()
+        cr.accept(cn, 0)
+
+        cn.interfaces.add(internalNameOf<URLClassLoaderAccessor>())
+        cn.visitMethod(Opcodes.ACC_PUBLIC, "addWeaveURL", "(Ljava/net/URL;)V", null, null).visitAsm {
+            aload(0)
+            aload(1)
+            invokevirtual(cn.name, "addURL", "(Ljava/net/URL;)V")
+            _return
+        }
+
+        val loadClass = cn.methods.find { it.name == "loadClass" && it.desc == "(Ljava/lang/String;Z)Ljava/lang/Class;" }
+        loadClass?.instructions?.insert(asm {
+            val end = LabelNode()
+            aload(1)
+            ldc("org.objectweb.asm.")
+            invokevirtual("java/lang/String", "startsWith", "(Ljava/lang/String;)Z")
+            ifeq(end)
+
+            aload(0)
+            aload(1)
+            iload(2)
+            invokespecial(cn.superName, "loadClass", "(Ljava/lang/String;Z)Ljava/lang/Class;")
+
+            +end
+            f_same()
+        })
+
         val cw = ClassWriter(cr, ClassWriter.COMPUTE_MAXS)
-
-        cr.accept(object : ClassVisitor(Opcodes.ASM9, cw) {
-            override fun visit(version: Int, access: Int, name: String?, signature: String?, superName: String?, interfaces: Array<String>) {
-                super.visit(version, access, name, signature, superName, interfaces + internalNameOf<URLClassLoaderAccessor>())
-            }
-
-            override fun visitEnd() {
-                val mv = super.visitMethod(Opcodes.ACC_PUBLIC, "addWeaveURL", "(Ljava/net/URL;)V", null, null)
-
-                mv.visitAsm {
-                    aload(0)
-                    aload(1)
-                    invokevirtual(cr.className, "addURL", "(Ljava/net/URL;)V")
-                    _return
-                }
-
-                mv.visitMaxs(2, 2)
-
-                super.visitEnd()
-            }
-        }, 0)
-
+        cn.accept(cw)
         return cw.toByteArray()
     }
 }
