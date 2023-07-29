62bdbb56b54e9e884b9d965f1b224b487c6153aa
exejar <56853437+exejar@users.noreply.github.com>
2023-07-29 01:51:02 -0400

    Compute max's

--- loader/src/main/kotlin/net/weavemc/loader/bootstrap/URLClassLoaderAccessor.kt
+++ loader/src/main/kotlin/net/weavemc/loader/bootstrap/URLClassLoaderAccessor.kt
@@ -1,15 +1,11 @@
 package net.weavemc.loader.bootstrap
 
-import net.weavemc.weave.api.bytecode.asm
 import net.weavemc.weave.api.bytecode.internalNameOf
 import net.weavemc.weave.api.bytecode.visitAsm
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes
-import org.objectweb.asm.tree.ClassNode
-import org.objectweb.asm.tree.MethodNode
-import java.io.FileOutputStream
 import java.net.URL
 import java.net.URLClassLoader
 
@@ -22,7 +18,7 @@ object URLClassLoaderTransformer : SafeTransformer {
         val cr = ClassReader(originalClass)
         if(cr.superName != internalNameOf<URLClassLoader>()) return null
 
-        val cw = ClassWriter(cr, 0)
+        val cw = ClassWriter(cr, ClassWriter.COMPUTE_MAXS)
 
         cr.accept(object : ClassVisitor(Opcodes.ASM9, cw) {
             override fun visit(version: Int, access: Int, name: String?, signature: String?, superName: String?, interfaces: Array<String>) {
@@ -30,12 +26,16 @@ object URLClassLoaderTransformer : SafeTransformer {
             }
 
             override fun visitEnd() {
-                super.visitMethod(Opcodes.ACC_PUBLIC, "addWeaveURL", "(Ljava/net/URL;)V", null, null).visitAsm {
+                val mv = super.visitMethod(Opcodes.ACC_PUBLIC, "addWeaveURL", "(Ljava/net/URL;)V", null, null)
+
+                mv.visitAsm {
                     aload(0)
                     aload(1)
                     invokevirtual(cr.className, "addURL", "(Ljava/net/URL;)V")
                     _return
                 }
+
+                mv.visitMaxs(2, 2)
 
                 super.visitEnd()
             }
