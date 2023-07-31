72e0b1c419539c7b2016dbc3633897432d3a185c
exejar <56853437+exejar@users.noreply.github.com>
2023-07-30 22:32:52 -0400

    Remap references of org/objectweb to net/weavemc

--- api/common/src/main/java/net/weavemc/weave/api/mapping/RemapperWrapper.kt
+++ api/common/src/main/java/net/weavemc/weave/api/mapping/RemapperWrapper.kt
@@ -3,7 +3,10 @@ package net.weavemc.weave.api.mapping
 import org.objectweb.asm.commons.Remapper
 
 open class RemapperWrapper(private val mapper: IMapper) : Remapper() {
-    override fun map(internalName: String): String? = mapper.mapClass(internalName) ?: internalName
+    override fun map(internalName: String): String? {
+        val name = internalName.replaceFirst("org/objectweb", "net/weavemc")
+        return mapper.mapClass(name) ?: name
+    }
 
     override fun mapMethodName(owner: String, name: String, descriptor: String): String? = mapper.mapMethod(owner, name, descriptor)?.name ?: name
 
