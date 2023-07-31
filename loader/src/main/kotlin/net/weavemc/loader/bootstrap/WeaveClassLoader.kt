ff1c773432161dcacbdc59abe04b325f677c65e8
exejar <56853437+exejar@users.noreply.github.com>
2023-07-30 22:27:33 -0400

    Added LaunchClassLoaderAccessor

--- loader/src/main/kotlin/net/weavemc/loader/bootstrap/WeaveClassLoader.kt
+++ loader/src/main/kotlin/net/weavemc/loader/bootstrap/WeaveClassLoader.kt
@@ -6,11 +6,16 @@ import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.ClassNode
+import java.io.FileOutputStream
 import java.net.URL
 import java.net.URLClassLoader
 
 interface URLClassLoaderAccessor {
     fun addWeaveURL(url: URL)
+}
+interface LaunchClassLoaderAccessor {
+    fun excludeFromClassLoader(pkg: String)
+    fun excludeFromTransformer(pkg: String)
 }
 
 object URLClassLoaderTransformer : SafeTransformer {
@@ -22,6 +27,10 @@ object URLClassLoaderTransformer : SafeTransformer {
         cr.accept(cn, 0)
 
         cn.interfaces.add(internalNameOf<URLClassLoaderAccessor>())
+        // Standard Minecraft ClassLoader
+        if (cr.className.contains("LaunchClassLoader"))
+            cn.interfaces.add(internalNameOf<LaunchClassLoaderAccessor>())
+
         cn.visitMethod(Opcodes.ACC_PUBLIC, "addWeaveURL", "(Ljava/net/URL;)V", null, null).visitAsm {
             aload(0)
             aload(1)
@@ -29,8 +38,23 @@ object URLClassLoaderTransformer : SafeTransformer {
             _return
         }
 
+        cn.visitMethod(Opcodes.ACC_PUBLIC, "excludeFromClassLoader", "(Ljava/lang/String;)V", null, null).visitAsm {
+            aload(0)
+            aload(1)
+            invokevirtual(cn.name, "addClassLoaderExclusion", "(Ljava/lang/String;)V")
+            _return
+        }
+
+        cn.visitMethod(Opcodes.ACC_PUBLIC, "excludeFromTransformer", "(Ljava/lang/String;)V", null, null).visitAsm {
+            aload(0)
+            aload(1)
+            invokevirtual(cn.name, "addTransformerExclusion", "(Ljava/lang/String;)V")
+            _return
+        }
+
         val cw = ClassWriter(cr, ClassWriter.COMPUTE_MAXS)
         cn.accept(cw)
+//        FileOutputStream(System.getProperty("user.home") + "/.weave/${className.replace("/", "_")}.class").use { it.write(cw.toByteArray()) }
         return cw.toByteArray()
     }
 }
