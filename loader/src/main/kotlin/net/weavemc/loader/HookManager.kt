44b65e570a00dff8b1893d709bcddcda4635574b
exejar <56853437+exejar@users.noreply.github.com>
2023-08-25 12:25:49 -0400

    Created HookClassWriter to retrieve common super class without class loading

--- loader/src/main/kotlin/net/weavemc/loader/HookManager.kt
+++ loader/src/main/kotlin/net/weavemc/loader/HookManager.kt
@@ -3,10 +3,12 @@ package net.weavemc.loader
 import net.weavemc.loader.bootstrap.SafeTransformer
 import net.weavemc.weave.api.Hook
 import net.weavemc.weave.api.bytecode.dump
+import net.weavemc.weave.api.gameVersion
+import net.weavemc.weave.api.mapping.XSrgRemapper
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.ClassNode
-import org.spongepowered.asm.mixin.transformer.ClassInfo
 import java.io.File
 import java.nio.file.Paths
 
@@ -37,9 +39,8 @@ internal object HookManager : SafeTransformer {
         val cfg = Hook.AssemblerConfig { computeFrames = true }
 
         hooks.forEach { it.transform(node, cfg) }
-        val flags = if (computeFrames) ClassWriter.COMPUTE_FRAMES else ClassWriter.COMPUTE_MAXS
+        val flags = if (computeFrames) ClassWriter.COMPUTE_FRAMES else ClassWriter.COMPUTE_FRAMES
 
-        // HACK: use MixinClassWriter because it doesn't load classes when computing frames.
         val writer = HookClassWriter(reader, flags)
         node.accept(writer)
         if (dumpBytecode) {
@@ -62,6 +63,48 @@ internal object HookManager : SafeTransformer {
     }
 }
 
-class HookClassWriter(classReader: ClassReader, flags: Int) : ClassWriter(classReader, flags) {
-    override fun getCommonSuperClass(type1: String, type2: String): String = ClassInfo.getCommonSuperClass(type1, type2).name
+class HookClassWriter(classReader: ClassReader, flags: Int): ClassWriter(classReader, flags) {
+    val vanillaRemapper = XSrgRemapper(gameVersion, "notch")
+    private fun getResourceAsByteArray(resourceName: String): ByteArray =
+        classLoader.getResourceAsStream("$resourceName.class")?.readBytes()
+            ?: classLoader.getResourceAsStream("${vanillaRemapper.mapClass(resourceName)}.class")?.readBytes()
+            ?: throw ClassNotFoundException("Failed to retrieve class from resources: $resourceName")
+    private fun ByteArray.asClassReader(): ClassReader = ClassReader(this)
+    private fun ClassReader.asClassNode(): ClassNode {
+        val cn = ClassNode()
+        this.accept(cn, 0)
+        return cn
+    }
+    private fun ClassNode.isInterface(): Boolean = (this.access and Opcodes.ACC_INTERFACE) != 0
+    private fun ClassReader.isAssignableFrom(target: ClassReader): Boolean {
+        val classes = ArrayDeque(listOf(target))
+
+        while (classes.isNotEmpty()) {
+            val cl = classes.removeFirst()
+            if (cl.className == this.className) return true
+
+            classes.addAll(
+                (listOfNotNull(cl.superName) + cl.interfaces).map { ClassReader(getResourceAsByteArray(it)) }
+            )
+        }
+
+        return false
+    }
+
+    override fun getCommonSuperClass(type1: String, type2: String): String {
+        var class1 = getResourceAsByteArray(type1).asClassReader()
+        val class2 = getResourceAsByteArray(type2).asClassReader()
+
+        return when {
+            class1.isAssignableFrom(class2) -> type1
+            class2.isAssignableFrom(class1) -> type2
+            class1.asClassNode().isInterface() || class2.asClassNode().isInterface() -> "java/lang/Object"
+            else -> {
+                while (!class1.isAssignableFrom(class2))
+                    class1 = getResourceAsByteArray(class1.superName).asClassReader()
+
+                return vanillaRemapper.reverseMapClass(class1.className) ?: class1.className
+            }
+        }
+    }
 }
