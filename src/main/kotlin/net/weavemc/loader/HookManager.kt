408d2c04d73f343b91850cee154a17089a242218
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-09-01 03:59:46 +0200

    Remove dumpBytecode

--- src/main/kotlin/net/weavemc/loader/HookManager.kt
+++ src/main/kotlin/net/weavemc/loader/HookManager.kt
@@ -3,24 +3,12 @@ package net.weavemc.loader
 import net.weavemc.loader.api.Hook
 import net.weavemc.loader.bootstrap.SafeTransformer
 import net.weavemc.loader.hooks.*
-import net.weavemc.loader.util.dump
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.tree.ClassNode
 import org.spongepowered.asm.transformers.MixinClassWriter
-import java.nio.file.Files
-import java.nio.file.Paths
 
 internal object HookManager : SafeTransformer {
-
-    /**
-     * JVM argument to dump bytecode to disk. Can be enabled by adding
-     * `-DdumpBytecode=true` to your JVM arguments when launching with Weave.
-     *
-     * Defaults to `false`.
-     */
-    val dumpBytecode = System.getProperty("dumpBytecode")?.toBoolean() ?: false
-
     val hooks = mutableListOf(
         ChatReceivedEventHook(),
         ChatSentEventHook(),
@@ -58,15 +46,6 @@ internal object HookManager : SafeTransformer {
         // HACK: use MixinClassWriter because it doesn't load classes when computing frames.
         val writer = MixinClassWriter(reader, flags)
         node.accept(writer)
-        if (dumpBytecode) {
-            val bytecodeDir = Files.createDirectory(Paths.get(
-                System.getProperty("user.home"),
-                ".weave",
-                ".bytecode.out"
-            ))
-
-            node.dump(bytecodeDir.resolve("$className.class").toString())
-        }
         return writer.toByteArray()
     }
 }
