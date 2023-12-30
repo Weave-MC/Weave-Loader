8c38583564756a45ea9f2d98377c91c9f8b17e49
exejar <56853437+exejar@users.noreply.github.com>
2023-12-29 19:00:44 -0500

    Removed GameInfoTransformer, and replaced with new Bootstrapping process

--- loader/src/main/kotlin/net/weavemc/loader/bootstrap/Agent.kt
+++ loader/src/main/kotlin/net/weavemc/loader/bootstrap/Agent.kt
@@ -1,9 +1,15 @@
 package net.weavemc.loader.bootstrap
 
+import net.weavemc.api.bytecode.asm
+import net.weavemc.api.bytecode.internalNameOf
+import net.weavemc.api.bytecode.named
 import net.weavemc.loader.WeaveLoader
+import net.weavemc.loader.asClassNode
+import net.weavemc.loader.asClassReader
 import net.weavemc.loader.bootstrap.transformer.AntiCacheTransformer
-import net.weavemc.loader.bootstrap.transformer.GameInfoTransformer
+import net.weavemc.loader.bootstrap.transformer.SafeTransformer
 import net.weavemc.loader.bootstrap.transformer.URLClassLoaderTransformer
+import org.objectweb.asm.ClassWriter
 import java.lang.instrument.Instrumentation
 
 /**
@@ -16,9 +22,34 @@ fun premain(opt: String?, inst: Instrumentation) {
 
     inst.addTransformer(URLClassLoaderTransformer)
     inst.addTransformer(AntiCacheTransformer)
-    inst.addTransformer(GameInfoTransformer)
 
-    inst.addTransformer(Bootstrap(inst))
+    inst.addTransformer(object: SafeTransformer {
+        override fun transform(loader: ClassLoader, className: String, originalClass: ByteArray): ByteArray? {
+            if (className != "net/minecraft/client/main/Main") return null
+
+            val reader = originalClass.asClassReader()
+            val node = reader.asClassNode()
+
+            with(node.methods.named("main")) {
+                instructions.insert(asm {
+                    ldc(className)
+                    aload(0)
+                    invokestatic(
+                        "net/weavemc/loader/bootstrap/BootstrapContainer",
+                        "bootstrapCallback",
+                        "(Ljava/lang/String;[Ljava/lang/String;)V"
+                    )
+                })
+            }
+
+            inst.removeTransformer(this)
+
+            return ClassWriter(reader, ClassWriter.COMPUTE_MAXS).also { node.accept(it) }.toByteArray()
+        }
+    })
+
+    // initialize bootstrap
+    Bootstrap(inst).also { BootstrapContainer.bootstrap = it }
 
     println("[Weave] Bootstrapped Weave")
 }
--- loader/src/main/kotlin/net/weavemc/loader/bootstrap/Bootstrap.kt
+++ loader/src/main/kotlin/net/weavemc/loader/bootstrap/Bootstrap.kt
@@ -1,86 +1,95 @@
 package net.weavemc.loader.bootstrap
 
-import net.weavemc.api.MinecraftClient
 import net.weavemc.api.gameClient
 import net.weavemc.api.gameVersion
 import net.weavemc.loader.FileManager
 import net.weavemc.loader.bootstrap.transformer.*
-import net.weavemc.loader.bootstrap.transformer.SafeTransformer
 import net.weavemc.loader.mapping.MappingsHandler
 import java.io.File
 import java.lang.instrument.Instrumentation
 import java.net.URL
 import java.util.jar.JarFile
 
-class Bootstrap(val inst: Instrumentation) : SafeTransformer {
-    override fun transform(loader: ClassLoader, className: String, originalClass: ByteArray): ByteArray? {
-        // Initialize Weave once the first Minecraft class is loaded into LaunchClassLoader (or main classloader for Minecraft)
-        if (
-            (gameClient != MinecraftClient.FORGE && className.startsWith("net/minecraft/client/")) ||
-            (gameClient == MinecraftClient.FORGE && className == "net/minecraftforge/fml/common/Loader")
-        ) {
-            println("[Weave] Detected Minecraft version: $gameVersion")
+object BootstrapContainer {
+    lateinit var bootstrap: Bootstrap
 
-            inst.removeTransformer(AntiCacheTransformer)
-            inst.removeTransformer(this)
+    @JvmStatic
+    fun bootstrap(caller: String, loader: ClassLoader, args: Array<String>) =
+        bootstrap.bootstrap(caller, loader, args)
 
-            val urlClassLoaderAccessor = if (loader is URLClassLoaderAccessor)
-                loader
-            else {
-                println("[Weave] Failed to transform URLClassLoader to implement URLClassLoaderAccessor. Defaulting to SCL Search")
-                object : URLClassLoaderAccessor {
-                    override fun addWeaveURL(url: URL) {
-                        inst.appendToSystemClassLoaderSearch(JarFile(url.file))
-                    }
+    @JvmStatic
+    fun bootstrapCallback(caller: String, args: Array<String>) {
+        Class.forName("net.weavemc.loader.bootstrap.BootstrapContainer", false, ClassLoader.getSystemClassLoader())
+            .getMethod("bootstrap", String::class.java, ClassLoader::class.java, Array<String>::class.java)
+            .invoke(null, caller, javaClass.classLoader, args)
+    }
+}
+
+class Bootstrap(val inst: Instrumentation) {
+    fun bootstrap(caller: String, loader: ClassLoader, args: Array<String>) {
+        // set game info arguments
+        System.getProperties()["weave.main.args"] = args.toList().chunked(2)
+            .associate { (a, b) -> a.removePrefix("--") to b }
+
+        println("[Weave] Bootstrapping...\n" +
+                "    -Caller: $caller\n" +
+                "    -Version: $gameVersion\n" +
+                "    -Client: $gameClient")
+
+        val urlClassLoaderAccessor = if (loader is URLClassLoaderAccessor)
+            loader
+        else {
+            println("[Weave] Failed to transform URLClassLoader to implement URLClassLoaderAccessor. Defaulting to SCL Search")
+            object : URLClassLoaderAccessor {
+                override fun addWeaveURL(url: URL) {
+                    inst.appendToSystemClassLoaderSearch(JarFile(url.file))
                 }
             }
-
-            fun File.createRemappedTemp(name: String): File {
-                val temp = File.createTempFile(name, "weavemod.jar")
-                MappingsHandler.remapModJar(
-                    MappingsHandler.fullMappings,
-                    this,
-                    temp,
-                    "official",
-                    MappingsHandler.environmentNamespace,
-                    listOf(FileManager.getVanillaMinecraftJar())
-                )
-                temp.deleteOnExit()
-                return temp
-            }
-
-            val versionApi = FileManager.getVersionApi()
-            val mods = FileManager.getMods().map { it.file }
-
-            val mappedVersionApi = versionApi?.createRemappedTemp("version-api")
-            val mappedMods = mods.map { it.createRemappedTemp(it.nameWithoutExtension) }
-
-            urlClassLoaderAccessor.addWeaveURL(FileManager.getCommonApi().toURI().toURL())
-            if (mappedVersionApi != null)
-                urlClassLoaderAccessor.addWeaveURL(mappedVersionApi.toURI().toURL())
-
-            mappedMods.forEach { urlClassLoaderAccessor.addWeaveURL(it.toURI().toURL()) }
-
-            removeTransformers()
-
-            /*
-            Load the rest of the loader using Minecraft's class loader.
-            This allows us to access Minecraft's classes throughout the project.
-            */
-            loader.loadClass("net.weavemc.loader.WeaveLoader")
-                .getDeclaredMethod("init", Instrumentation::class.java, File::class.java, List::class.java)
-                .invoke(null, inst, mappedVersionApi, mappedMods)
         }
 
-        return null
+        fun File.createRemappedTemp(name: String): File {
+            val temp = File.createTempFile(name, "weavemod.jar")
+            MappingsHandler.remapModJar(
+                MappingsHandler.environmentMappings.mappings,
+                this,
+                temp,
+                "official",
+                "named",
+                listOf(FileManager.getVanillaMinecraftJar())
+            )
+            println(temp.path)
+//            temp.deleteOnExit()
+            return temp
+        }
+
+        val versionApi = FileManager.getVersionApi()
+        val mods = FileManager.getMods().map { it.file }
+
+        val mappedVersionApi = versionApi?.createRemappedTemp("version-api")
+        val mappedMods = mods.map { it.createRemappedTemp(it.nameWithoutExtension) }
+
+        urlClassLoaderAccessor.addWeaveURL(FileManager.getCommonApi().toURI().toURL())
+        if (mappedVersionApi != null)
+            urlClassLoaderAccessor.addWeaveURL(mappedVersionApi.toURI().toURL())
+
+        mappedMods.forEach { urlClassLoaderAccessor.addWeaveURL(it.toURI().toURL()) }
+
+        removeTransformers()
+
+        println("[Weave] Bootstrapping complete.")
+        /*
+        Load the rest of the loader using Minecraft's class loader.
+        This allows us to access Minecraft's classes throughout the project.
+        */
+        loader.loadClass("net.weavemc.loader.WeaveLoader")
+            .getDeclaredMethod("init", Instrumentation::class.java, File::class.java, List::class.java)
+            .invoke(null, inst, mappedVersionApi, mappedMods)
     }
 
     private fun removeTransformers() {
         println("[Weave] Removing Bootstrapping Transformers")
-        inst.removeTransformer(this)
         inst.removeTransformer(AntiCacheTransformer)
         inst.removeTransformer(URLClassLoaderTransformer)
-        inst.removeTransformer(GameInfoTransformer)
         println("[Weave] Removed Bootstrapping Transformers")
     }
 }
