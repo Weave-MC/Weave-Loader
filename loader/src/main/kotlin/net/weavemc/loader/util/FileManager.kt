5d2ea2658081ae4ca9f7652220577a6eec3c9c78
770grappenmaker <770grappenmaker@gmail.com>
2024-03-27 15:11:29 +0100

    temp: stuff is still somewhat incoherent

--- loader/src/main/kotlin/net/weavemc/loader/util/FileManager.kt
+++ loader/src/main/kotlin/net/weavemc/loader/util/FileManager.kt
@@ -1,6 +1,7 @@
 package net.weavemc.loader.util
 
 import net.weavemc.internals.GameInfo.gameVersion
+import net.weavemc.internals.MinecraftVersion
 import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
