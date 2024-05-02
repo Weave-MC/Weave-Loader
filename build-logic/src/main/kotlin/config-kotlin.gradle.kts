395b2e122abe6d03da67305467ff94c8292cefe1
xtrm <oss@xtrm.me>
2024-05-02 14:56:40 +0200

    🐛 fix(build-logic): it was all imported by default anyways

--- build-logic/src/main/kotlin/config-kotlin.gradle.kts
+++ build-logic/src/main/kotlin/config-kotlin.gradle.kts
@@ -1,19 +1,10 @@
-import gradle.kotlin.dsl.accessors._d73e0aad27c892ff134862f94ec3182c.base
-import gradle.kotlin.dsl.accessors._d73e0aad27c892ff134862f94ec3182c.java
-import gradle.kotlin.dsl.accessors._d73e0aad27c892ff134862f94ec3182c.kotlin
-import org.gradle.kotlin.dsl.`java-library`
-import org.gradle.kotlin.dsl.kotlin
-import org.gradle.kotlin.dsl.repositories
-
 plugins {
     `java-library`
     kotlin("jvm")
     kotlin("plugin.serialization")
 }
 
-val toolchainTarget = Action<JavaToolchainSpec> {
-    languageVersion.set(JavaLanguageVersion.of(8))
-}
+val toolchainTarget = JavaLanguageVersion.of(8)
 
 repositories {
     mavenCentral()
@@ -23,7 +14,14 @@ base {
     archivesName.set("weave-${project.name}")
 }
 
-java.withSourcesJar()
+java {
+    withSourcesJar()
 
-toolchainTarget.execute(java.toolchain)
-kotlin.jvmToolchain(toolchainTarget)
+    toolchain {
+        languageVersion = toolchainTarget
+    }
+}
+
+kotlin.jvmToolchain {
+    languageVersion = toolchainTarget
+}
