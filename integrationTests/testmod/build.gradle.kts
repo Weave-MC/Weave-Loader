d036e9c113468eee11d61e4cc2932d9b50afe217
xtrm <oss@xtrm.me>
2024-05-06 05:27:05 +0200

    🐛 fix(integrationTests/testmod): fix compilation issue with Weave-Gradle 1.0.0-PRE

--- integrationTests/testmod/build.gradle.kts
+++ integrationTests/testmod/build.gradle.kts
@@ -1,16 +1,24 @@
-import net.weavemc.gradle.configuration.WeaveMinecraftExtension
-
 plugins {
     id("net.weavemc.gradle") version "1.0.0-PRE"
 }
 
-configure<WeaveMinecraftExtension> {
+group = "net.weavemc.loader"
+version = "1.0.0"
+
+minecraft {
     configure {
         name = "Test Mod"
         modId = "testmod"
-//        entryPoints = listOf("com.example.mod.ExampleMod")
-//        mixinConfigs = listOf("examplemod.mixins.json")
-        mcpMappings()
+        entryPoints = listOf("net.weavemc.loader.tests.TestMod")
+        mojangMappings()
     }
     version("1.8.9")
 }
+
+dependencies {
+    implementation(project(":loader"))
+}
+
+// Workaround 1.0.0-PRE bug
+tasks["remapJar"].enabled = false
+tasks["processResources"].outputs.upToDateWhen { false }
---
+++ integrationTests/testmod/src/main/java/net/weavemc/loader/tests/TestMod.kt
@@ -0,0 +1,9 @@
+package net.weavemc.loader.tests
+
+import net.weavemc.api.ModInitializer
+
+class TestMod : ModInitializer {
+    override fun init() {
+        println("Hello, world!")
+    }
+}
