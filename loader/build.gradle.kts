6be7f8bae002f0dfe0816bcf11e2381ab76d81bf
xtrm <oss@xtrm.me>
2024-08-03 23:22:14 +0200

    Revert "✨ feature(loader): add an `apiJar` task, add manifest attributes for packages spec/impl"
    This reverts commit e1000556825e0f109474389fa23d3749111de493.

--- loader/build.gradle.kts
+++ loader/build.gradle.kts
@@ -29,55 +29,20 @@ dependencies {
     }
 }
 
-tasks.jar {
-    exclude("net/weavemc/loader/api/**")
-    manifest.attributes(
-        "Premain-Class" to "net.weavemc.loader.impl.bootstrap.AgentKt",
-        "Main-Class" to "net.weavemc.loader.impl.bootstrap.AgentKt",
-        "Can-Retransform-Classes" to "true",
-
-        "Automatic-Module-Name" to "net.weavemc.loader",
-    )
-
-    manifest.attributes(
-        mapOf(
-            "Specification-Title" to "Weave Loader",
-            "Specification-Version" to "0", // we're still in beta so this is 0
-            "Specification-Vendor" to "WeaveMC",
-            "Implementation-Title" to "Weave Loader",
-            "Implementation-Version" to project.version,
-            "Implementation-Vendor" to "WeaveMC",
-        ), "net.weavemc.loader"
-    )
-}
-
-val apiJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("api")
-    from(sourceSets.main.get().output)
-    include("net/weavemc/loader/api/**")
-
-    manifest.attributes(
-        mapOf(
-            "Specification-Title" to "Weave Loader API",
-            "Specification-Version" to "0",
-            "Specification-Vendor" to "WeaveMC",
-            "Implementation-Title" to "Weave Loader API",
-            "Implementation-Version" to project.version,
-            "Implementation-Vendor" to "WeaveMC",
-        ), "net.weavemc.loader.api"
-    )
-}
-
-artifacts {
-    archives(apiJar)
+tasks {
+    jar {
+        manifest.attributes(
+            "Premain-Class" to "net.weavemc.loader.impl.bootstrap.AgentKt",
+            "Main-Class" to "net.weavemc.loader.impl.bootstrap.AgentKt",
+            "Can-Retransform-Classes" to "true",
+        )
+    }
 }
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
-            artifact(apiJar)
-
             groupId = "net.weavemc"
             artifactId = "loader"
             version = "${project.version}"
