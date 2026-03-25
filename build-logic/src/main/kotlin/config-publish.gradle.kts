7a43dcc56eda0478a6aeee80cc5ea1b5fa7d644b
Candicey <13133556-Candicey@users.noreply.gitlab.com>
2026-03-25 17:18:21 +0700

    Add GitLab CI/CD integration

--- build-logic/src/main/kotlin/config-publish.gradle.kts
+++ build-logic/src/main/kotlin/config-publish.gradle.kts
@@ -7,7 +7,9 @@ publishing {
         maven {
             name = "WeaveMC"
             url = uri("https://repo.weavemc.dev/releases")
+
             credentials(PasswordCredentials::class)
+
             authentication {
                 create<BasicAuthentication>("basic")
             }
@@ -16,6 +18,7 @@ publishing {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/Weave-MC/Weave-Loader")
+
             credentials {
                 username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
                 password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.key") as String?
@@ -24,13 +27,17 @@ publishing {
 
         maven {
             name = "GitLabPackageRegistry"
-            url = uri("https://gitlab.com/api/v4/projects/80566527/packages/maven") // https://gitlab.com/weave-mc/weave
+            val projectId = System.getenv("CI_PROJECT_ID") ?: "80566527"
+            url = uri("${System.getenv("CI_API_V4_URL") ?: "https://gitlab.com/api/v4"}/projects/$projectId/packages/maven")
+
             credentials(HttpHeaderCredentials::class) {
-                name = "Private-Token"
-                value = findProperty("gitLabPrivateToken") as String?
+                val isCi = System.getenv("CI_JOB_TOKEN") != null
+                name = if (isCi) "Job-Token" else "Private-Token"
+                value = System.getenv("CI_JOB_TOKEN") ?: (findProperty("gitLabPrivateToken") as String?)
             }
+
             authentication {
-                create("header", HttpHeaderAuthentication::class)
+                create<HttpHeaderAuthentication>("header")
             }
         }
 
